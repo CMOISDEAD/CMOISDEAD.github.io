ef1be73ca9e5a7f5fc8ca5e87eb26d6f2acc680a
CMOISDEAD <camilodavina@gmail.com>
2021-03-05 15:54:41 -0500

    better ui?

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './css/main.scss'
 
 console.log("Imported")
+
+var gatillo = document.getElementById("gatillo")
+var sider = document.getElementById("sider")
+
+
+gatillo.addEventListener("click", ()=>{
+    if ( sider.className.match(/(?:^|\s)lado(?!\S)/) ){
+        sider.className =sider.className.replace( /(?:^|\s)lado(?!\S)/g , 'sider' )
+    }else{
+        sider.className =sider.className.replace( /(?:^|\s)sider(?!\S)/g , 'lado' )
+    }
+})
