c22cd0bd73266abcb36d5e921e2304a2acd0caff
CMOISDEAD <camilodavina@gmail.com>
2021-03-06 17:48:58 -0500

    better ui?

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ console.log("Imported")
 var gatillo = document.getElementById("gatillo")
 var sider = document.getElementById("sider")
 
-
 gatillo.addEventListener("click", ()=>{
     if ( sider.className.match(/(?:^|\s)lado(?!\S)/) ){
         sider.className =sider.className.replace( /(?:^|\s)lado(?!\S)/g , 'sider' )
