f57e4f0c52bd26a703405e71e86431279e0881d0
CMOISDEAD <camilodavina@gmail.com>
2021-03-07 18:39:21 -0500

    hola

--- src/index.js
+++ src/index.js
@@ -3,9 +3,18 @@ import './css/main.scss'
 console.log("Imported")
 
 var gatillo = document.getElementById("gatillo")
+var gatillo2 = document.getElementById("gatillo2")
 var sider = document.getElementById("sider")
 
 gatillo.addEventListener("click", ()=>{
+    if ( sider.className.match(/(?:^|\s)lado(?!\S)/) ){
+        sider.className =sider.className.replace( /(?:^|\s)lado(?!\S)/g , 'sider' )
+    }else{
+        sider.className =sider.className.replace( /(?:^|\s)sider(?!\S)/g , 'lado' )
+    }
+})
+
+gatillo2.addEventListener("click", ()=>{
     if ( sider.className.match(/(?:^|\s)lado(?!\S)/) ){
         sider.className =sider.className.replace( /(?:^|\s)lado(?!\S)/g , 'sider' )
     }else{
